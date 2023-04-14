efe17e4c2e13f05d91ad7f35da8ea941a6724cf7
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2023-04-14 23:38:17 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,12 @@ import { ToastContainer } from 'react-toastify'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className='flex flex-col min-h-screen bg-white text-black'>
-      <ToastContainer limit={1}/>
+    <div className='flex flex-col min-h-screen bg-white text-black'>      
       <div className='flex justify-center'>
         <Navbar />
       </div>
       <main className='flex-grow py-10 md:pt-40 lg:pt-20 px-10'>        
+        <ToastContainer limit={1}/>
         <Component {...pageProps} />
       </main>
       <footer className='mt-auto'>
