f93cddd8a61213cf678d59a2e2071888955b4ad0
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2023-04-14 22:56:23 +0800

    Update votingtime.js

--- pages/votingtime.js
+++ pages/votingtime.js
@@ -1,26 +1,26 @@
 import Image from 'next/image';
 
 // images
-import Messi from '../public/legend-images/players/messi.png';
-import CR7 from '../public/legend-images/players/CR7.png';
-import Pele from '../public/legend-images/players/Pele.png';
-import Maradona from '../public/legend-images/players/maradona.png';
-import JohanCruyff from '../public/legend-images/players/cruyff.png';
-import ZinedineZidane from '../public/legend-images/players/zidane.png';
-import AlfredoDiStefano from '../public/legend-images/players/alfredo-di-stéfano.png';
-import Garrincha from '../public/legend-images/players/garrincha.png';
-import R9 from '../public/legend-images/players/R9.png';
-import Ronaldinho from '../public/legend-images/players/ronaldinho.png';
-import Platini from '../public/legend-images/players/platini.png'
-import Beckenbauer from '../public/legend-images/players/beckenbauer.png';
-import GeorgeBest from '../public/legend-images/players/george-best.png';
-import Iniesta from '../public/legend-images/players/iniesta.png';
-import Xavi from '../public/legend-images/players/xavi.png';
-import Maldini from '../public/legend-images/players/maldini.png';
+import Messi from '/public/legend-images/players/messi.png';
+import CR7 from '/public/legend-images/players/CR7.png';
+import Pele from '/public/legend-images/players/Pele.png';
+import Maradona from '/public/legend-images/players/maradona.png';
+import JohanCruyff from '/public/legend-images/players/cruyff.png';
+import ZinedineZidane from '/public/legend-images/players/zidane.png';
+import AlfredoDiStefano from '/public/legend-images/players/alfredo-di-stéfano.png';
+import Garrincha from '/public/legend-images/players/garrincha.png';
+import R9 from '/public/legend-images/players/R9.png';
+import Ronaldinho from '/public/legend-images/players/ronaldinho.png';
+import Platini from '/public/legend-images/players/platini.png'
+import Beckenbauer from '/public/legend-images/players/beckenbauer.png';
+import GeorgeBest from '/public/legend-images/players/george-best.png';
+import Iniesta from '/public/legend-images/players/iniesta.png';
+import Xavi from '/public/legend-images/players/xavi.png';
+import Maldini from '/public/legend-images/players/maldini.png';
 import GerdMuller from '../public/legend-images/players/gerd-muller.png';
-import Baggio from '../public/legend-images/players/baggio.png';
-import Yashin from '../public/legend-images/players/yashin.png';
-import Ferenc from '../public/legend-images/players/ferenc-puskás.png'
+import Baggio from '/public/legend-images/players/baggio.png';
+import Yashin from '/public/legend-images/players/yashin.png';
+import Ferenc from '/public/legend-images/players/ferenc-puskás.png'
 
 import Head from "next/head";
 import { auth, db } from "../utils/firebase";
