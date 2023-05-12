456eb312802882b52161aa3f488412dd12e80581
Dun Yan <ong.dy2004@gmail.com>
2023-05-12 17:47:42 +0800

    setting description

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import DeleteAccount from "../components/DeleteAcount";
+import { auth } from "../utils/firebase";
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Dashboard = () => {
+    const [user, loading] = useAuthState(auth);    
     return (
         <div className="md:px-5 md:py-30 w-full max-w-2xl mx-auto pt-20">
             <Head>
@@ -18,7 +21,7 @@ const Dashboard = () => {
             <div className="flex items-center justify-between px-2">
                 <div className="grid gap-1">
                     <h1 className="font-bold text-3xl md:text-4xl">Settings</h1>
-                    <p className="text-lg text-muted-foreground">Manage account and website settings.</p>
+                    <p className="py-5 text-lg text-muted-foreground">Hello <span className="font-semibold">{user && user.displayName}</span>, welcome to your account and website settings. Here, you can manage various aspects of your account and website to ensure they are tailored to your preferences and needs.</p>
                 </div>
             </div>
             <DeleteAccount />
